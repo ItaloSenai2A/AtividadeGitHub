a914f94e008dd2ba085bd6cde9377b64e80b31d6
Rayssa <rayssa.senaiii@gmail.com>
2025-02-11 16:46:26 -0300

    Alterações em Emanuelly e Italo

--- Controllers/Emanuelly.cs
+++ Controllers/Emanuelly.cs
@@ -3,5 +3,6 @@
     public class Emanuelly
     {
         public int TesteEmanuelly { get; set; }
+        public int AlteracoesRayssa { get; set; }
     }
 }
--- Controllers/Italo.cs
+++ Controllers/Italo.cs
@@ -3,5 +3,6 @@
     public class Italo
     {
         public int TesteItalo { get; set; }
+        public int AlteracoesRayssa { get; set; }
     }
 }
